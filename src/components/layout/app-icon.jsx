86d479dd330a50aaa029b99f68d34183072c295e
dannegm <im@danielgarcia.me>
2026-06-18 15:40:23 -0600

    feat: wrap AppIcon component in Link for improved navigation

--- src/components/layout/app-icon.jsx
+++ src/components/layout/app-icon.jsx
@@ -1,19 +1,22 @@
 import { cn } from '@/helpers/utils';
+import { Link } from '@tanstack/react-router';
 
 export const AppIcon = ({ className }) => (
-    <div
-        className={cn(
-            'grid grid-cols-2 grid-rows-2 gap-0 size-8 p-1 overflow-hidden squircle-xl bg-brand',
-            className,
-        )}
-    >
-        {['B', 'I', 'N', 'S'].map(l => (
-            <span
-                key={l}
-                className='flex-center font-mono text-[11px] font-bold leading-none text-brand-foreground'
-            >
-                {l}
-            </span>
-        ))}
-    </div>
+    <Link to='/' className={cn('flex items-center justify-center', className)}>
+        <div
+            className={cn(
+                'grid grid-cols-2 grid-rows-2 gap-0 size-8 p-1 overflow-hidden squircle-xl bg-brand',
+                className,
+            )}
+        >
+            {['B', 'I', 'N', 'S'].map(l => (
+                <span
+                    key={l}
+                    className='flex-center font-mono text-[11px] font-bold leading-none text-brand-foreground'
+                >
+                    {l}
+                </span>
+            ))}
+        </div>
+    </Link>
 );
