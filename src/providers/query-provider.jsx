a7d7f64b1aa4b185a4cb41142248921d1bbd8c2f
dannegm <im@danielgarcia.me>
2026-06-18 17:40:57 -0600

    feat: configure QueryClient with default options for query behavior

--- src/providers/query-provider.jsx
+++ src/providers/query-provider.jsx
@@ -1,6 +1,13 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: true,
+            staleTime: 0,
+        },
+    },
+});
 
 export const QueryProvider = ({ children }) => (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
