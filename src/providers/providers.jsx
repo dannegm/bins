bca46b7dd725d9ed687e13510f5a302abc2969b1
dannegm <im@danielgarcia.me>
2026-06-22 17:11:52 -0600

    feat: enable maintenance mode in providers

--- src/providers/providers.jsx
+++ src/providers/providers.jsx
@@ -21,7 +21,7 @@ import { NyanCatProvider } from '@/components/system/nyan-cat';
 import { DeviceProvider } from '@/providers/device-provider';
 import { GlobalDropzoneProvider } from '@/providers/global-dropzone-provider';
 
-const MAINTENANCE_MODE = false;
+const MAINTENANCE_MODE = true;
 
 const CommandsBridge = ({ children }) => {
     useExternalCommands();
